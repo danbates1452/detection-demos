1b31647e2a0ee0a8321ae31c9410fbca33591911
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2023-06-01 02:04:51 +0100

    more work on interactive segmentation
    may have to give up as it doesn't like video

--- interactive_segmentation.py
+++ interactive_segmentation.py
@@ -35,24 +35,6 @@ def get_image(capture, width=model_width, height=model_height):
     image = cv2.resize(frame, (width, height))
     return image
 
-def _normalized_to_pixel_coordinates(
-    normalized_x: float, normalized_y: float, image_width: int,
-    image_height: int):
-  """Converts normalized value pair to pixel coordinates."""
-
-  # Checks if the float value is between 0 and 1.
-  def is_valid_normalized_value(value: float) -> bool:
-    return (value > 0 or math.isclose(0, value)) and (value < 1 or
-                                                      math.isclose(1, value))
-
-  if not (is_valid_normalized_value(normalized_x) and
-          is_valid_normalized_value(normalized_y)):
-    # TODO: Draw coordinates even if it's outside of the image bounds.
-    return None
-  x_px = min(math.floor(normalized_x * image_width), image_width - 1)
-  y_px = min(math.floor(normalized_y * image_height), image_height - 1)
-  return x_px, y_px
-
 options = InteractiveSegmenterOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     output_category_mask=True
@@ -67,8 +49,8 @@ cv2.setMouseCallback(window_name, handle_click)
 with InteractiveSegmenter.create_from_options(options) as segmenter:
     while True:
         image = get_image(video_capture)
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB) #switch blue-green-red image to red-green-blue
-
+        image = cv2.flip(image, 1)
+        
         fps = video_capture.get(cv2.CAP_PROP_FPS)
 
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
@@ -77,17 +59,16 @@ with InteractiveSegmenter.create_from_options(options) as segmenter:
 
         category_mask = result.category_mask
 
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) #return RGB image to BGR for presentation
-
-        #TODO: smooth and use transparent colour
-        image_data = image
-        fg_image = np.zeros(image_data.shape, dtype=np.uint8)
-        fg_image[:] = np.flip(overlay_colour)
+        #image_data = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #switch blue-green-red image to red-green-blue
+        image_data = copy.deepcopy(image)
+        overlay_image = np.zeros(image_data.shape, dtype=np.uint8)
+        overlay_image[:] = np.flip(overlay_colour)
         
-        alpha = 0.5
-        fg_image = cv2.addWeighted(image, 1 - alpha, fg_image, alpha, 0)
-        condition = np.stack((category_mask.numpy_view(),) * 3, axis=-1) > 0.1
-        image_out = np.where(condition, image_data, fg_image)
+        alpha = np.stack((category_mask.numpy_view(),) * 3, axis=-1) > 0.1
+        alpha = alpha.astype(float) * 0.7
+
+        image_out = image_data * (1 - alpha) + overlay_image * alpha
+        image_out = image_out.astype(np.uint8)
 
         # Draw a white dot with black border to denote the point of interest
         thickness, radius = 6, -1
